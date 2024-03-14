91f45eaf99f8cc5ca92f24caa1581d4bb30d3fe2
x2Neptune <94178333+ZoiIntra@users.noreply.github.com>
2024-03-14 18:23:37 +0700

    Update __UI.lua

--- __UI.lua
+++ __UI.lua
@@ -9,7 +9,7 @@ local RenderStepped = RunService.RenderStepped;
 local LocalPlayer = Players.LocalPlayer;
 local Mouse = LocalPlayer:GetMouse();
 
-if CoreGui:FindFirstChild("Kui") then CoreGui:FindFirstChild("Kui"):Destroy() end
+if CoreGui:FindFirstChild("Kui_AiSus") then CoreGui:FindFirstChild("Kui_AiSus"):Destroy() end
 
 local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
 
@@ -18,7 +18,7 @@ ProtectGui(ScreenGui);
 
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global;
 ScreenGui.Parent = CoreGui;
-ScreenGui.Name = "Kui"
+ScreenGui.Name = "Kui_AiSus"
 
 local Toggles = {};
 local Options = {};
@@ -55,8 +55,8 @@ local Library = {
 local RainbowStep = 0
 local Hue = 0
 
-table.insert(Library.Signals, RenderStepped:Connect(function(Kui)
-    RainbowStep = RainbowStep + Kui
+table.insert(Library.Signals, RenderStepped:Connect(function(Kui_AiSus)
+    RainbowStep = RainbowStep + Kui_AiSus
 
     if RainbowStep >= (1 / 60) then
         RainbowStep = 0
